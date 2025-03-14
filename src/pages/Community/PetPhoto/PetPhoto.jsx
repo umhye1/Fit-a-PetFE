2fee7b911f1306a2982494d53025dc402741a256
umhye1 <lucy021206@gmail.com>
2025-03-15 01:29:27 +0900

    ui/community - 커뮤니티 수정

--- src/pages/Community/PetPhoto/PetPhoto.jsx
+++ src/pages/Community/PetPhoto/PetPhoto.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
+import styled from 'styled-components';
 import RecommendWalkMate from '../RecommendWalkMate/RecommendWalkMate';
+import { Card } from '../../../components/Card/Card';
+
+const CommunityContainer = styled.div`
+  display: flex;
+  width: 100%;
+  background-color: #FBFFE4;
+  flex-direction: column;
+
+`;
+
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: white;
+
+`;
+
+const CategroyContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 13.4vw;
+    padding: 1.5vw 0vw 0vw 5.8vw;
+    flex-shrink: 0;
+`;
+
+const CategroyP = styled.div`
+    display: flex;
+    padding: 0.7vw;
+    font-weight: 600;
+    font-size: 1vw;
+    &:visited { 
+        color: #3D8D7A;
+    }
+    &:hover { 
+        color: #3D8D7A; 
+    }
+    &:active { 
+        color: #3D8D7A;
+    }
+`;
+
+const PetPhotoContainer= styled.div`
+    width: 100%;
+    background-color: lightgray;
+    width: calc(100% - 19.2vw);
+`;
+
+
 
 const PetPhoto = () => {
   return (
-    <div>PetPhoto</div>
+    <CommunityContainer>
+        <RecommendWalkMate/>
+        <MainContainer>
+            <CategroyContainer>
+                <CategroyP>인기글</CategroyP>
+            </CategroyContainer>
+            <PetPhotoContainer>
+                <Card/>
+            </PetPhotoContainer>
+
+        </MainContainer>
+    </CommunityContainer>
   )
 }
 
